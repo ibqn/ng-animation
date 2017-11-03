4d737c83f32b0d7ca77d15fd8505ff68928dda1c
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-11-03 17:19:20 +0100

    add spoons

--- src/app/advanced/advanced.component.ts
+++ src/app/advanced/advanced.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Spoon {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-advanced',
@@ -7,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advanced.component.sass']
 })
 export class AdvancedComponent implements OnInit {
+  private _spoons: Spoon[];
+
+  public get spoons() { return this._spoons; }
 
   constructor() { }
 
   ngOnInit() {
+    this._spoons = [
+      { id: 1, name: 'Absinthe' },
+      { id: 2, name: 'Bouillon' },
+      { id: 3, name: 'Caviar' },
+      { id: 4, name: 'Coffee' },
+      { id: 5, name: 'Dessert' },
+      { id: 6, name: 'Egg' },
+      { id: 7, name: 'Horn' },
+      { id: 8, name: 'Iced Tea' },
+      { id: 9, name: 'Marrow' },
+      { id: 10, name: 'Melon' },
+      { id: 11, name: 'Parfait' },
+      { id: 12, name: 'Salt' },
+      { id: 13, name: 'Soup' },
+    ];
   }
 
 }
